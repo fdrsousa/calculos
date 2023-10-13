6ac8223e239e2a5b934fd92328ccfd83d7751b6e
Fdrsousa <38611439+fdrsousa@users.noreply.github.com>
2023-10-13 19:30:11 +0100

    Criar a classe Conversoes e mover método para a nova classe.
    O método ConverterTemperatura foi movido da classe Aritmetica para a classe Conversoes dado que implementa uma operação que não se adequa à classe original.

--- Aritmetica.cs
+++ Aritmetica.cs
@@ -12,16 +12,7 @@ namespace Calculos
     internal static class Aritmetica
 
     {
-        /// <summary>
-        /// Tipo de conversão de temperatura a executar.
-        /// </summary>
-        public enum ConversaoTemperatura
-        {
-            Nulo = 0,
-            CelsiusFahrenheit,
-            FahrenheitCelsius
-
-        }
+        
         /// <summary>
         /// Operação Soma.
         /// </summary>
@@ -39,18 +30,6 @@ namespace Calculos
             return n1 - n2;
         }
 
-        public static double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
-        {
-            if (conversao == ConversaoTemperatura.CelsiusFahrenheit)
-            {
-                return (temperatura * 1.8000 + 32);
-            }
-            else if (conversao == ConversaoTemperatura.FahrenheitCelsius)
-            {
-                return (temperatura - 32) / 1.8000;
-            }
-            else
-                return -1;
-        }
+
     }
 }
